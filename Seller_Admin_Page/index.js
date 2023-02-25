d21bff7d10c6693de8e30f581309378c2f76cfc3
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-02-25 15:44:43 +0530

    async await

--- Seller_Admin_Page/index.js
+++ Seller_Admin_Page/index.js
@@ -8,38 +8,37 @@ const tableData = document.createElement('li');
 var id;
 
 // // POST DATA
-function sellData(){
-    var sellprice = document.getElementById('sellprice').value;
-    var productname = document.getElementById('productname').value;
-    var category = document.getElementById('category').value;
-    let sellerData = {
-        sellprice,
-        productname,
-        category
+async function sellData(){
+    try{
+        var sellprice = document.getElementById('sellprice').value;
+        var productname = document.getElementById('productname').value;
+        var category = document.getElementById('category').value;
+        let sellerData = {
+            sellprice,
+            productname,
+            category
+        }
+        let response = await axios.post('https://crudcrud.com/api/ad04ac078ebc4d6b9901af19d427f49a/AppData', sellerData);
+        windowData(response.data);
+        location.reload();  
+    }catch(error){
+        document.getElementById('error').innerHTML = `${error.message}`;
     }
-    axios.post('https://crudcrud.com/api/e5b68ad560404240932de306ee9dc965/AppData', sellerData)
-    .then((response)=>{
-        console.log(response);
-        location.reload();
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-
 }
 
 // GET DATA
 getData();
-function getData(){
-    axios.get('https://crudcrud.com/api/e5b68ad560404240932de306ee9dc965/AppData')
-    .then((response)=>{
-        for(let i = 0; i < response.data.length; i++){
-            windowData(response.data[i]);
+
+async function getData(){
+    try{
+        let sellerSell = await axios.get('https://crudcrud.com/api/ad04ac078ebc4d6b9901af19d427f49a/AppData');
+        // console.log(sellerSell);
+        for(let data of sellerSell.data){
+            windowData(data);
         }
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+    }catch(error){
+        console.log(error);
+    }
 }
 
 
@@ -52,15 +51,16 @@ function windowData(response){
     btn.value = 'delete';
 
     // Delete USER DATA
-    btn.onclick = () =>{
-        axios
-        .delete('https://crudcrud.com/api/e5b68ad560404240932de306ee9dc965/AppData/'+response._id)
-        .then((response)=>{
-            location.reload();
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+    btn.onclick =  async () =>{
+        try{
+            let deleteData = await axios.delete('https://crudcrud.com/api/ad04ac078ebc4d6b9901af19d427f49a/AppData/'+response._id);
+            if(deleteData.status == 200){
+                location.reload();
+            }
+        
+        }catch(error){
+            console.log(error);
+        }
     }
     
     tableData.appendChild(btn);
